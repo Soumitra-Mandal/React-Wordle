7a3e1de3fb5656a6864ac0c566d44781f3b82211
Soumitra Mandal <soumitramandal@gmail.com>
2022-12-29 21:48:32 +0530

    Added basic grid for wordle

---
+++ src/components/TextGrid.js
@@ -0,0 +1,43 @@
+function TextGrid() {
+  return (
+    <div id="textgrid">
+      <div className="row1 row">
+        <div className="item1 item"></div>
+        <div className="item2 item"></div>
+        <div className="item3 item"></div>
+        <div className="item4 item"></div>
+        <div className="item5 item"></div>
+      </div>
+      <div className="row2 row">
+        <div className="item1 item"></div>
+        <div className="item2 item"></div>
+        <div className="item3 item"></div>
+        <div className="item4 item"></div>
+        <div className="item5 item"></div>
+      </div>
+      <div className="row3 row">
+        <div className="item1 item"></div>
+        <div className="item2 item"></div>
+        <div className="item3 item"></div>
+        <div className="item4 item"></div>
+        <div className="item5 item"></div>
+      </div>
+      <div className="row4 row">
+        <div className="item1 item"></div>
+        <div className="item2 item"></div>
+        <div className="item3 item"></div>
+        <div className="item4 item"></div>
+        <div className="item5 item"></div>
+      </div>
+      <div className="row5 row">
+        <div className="item1 item"></div>
+        <div className="item2 item"></div>
+        <div className="item3 item"></div>
+        <div className="item4 item"></div>
+        <div className="item5 item"></div>
+      </div>
+    </div>
+  );
+}
+
+export default TextGrid;

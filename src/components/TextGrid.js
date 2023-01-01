a59b8948b09f29faeb82d75c888b4c97ac43310e
Soumitra Mandal <soumitramandal@gmail.com>
2023-01-01 22:57:05 +0530

    Added word input and shifting to next row.

--- src/components/TextGrid.js
+++ src/components/TextGrid.js
@@ -1,11 +1,30 @@
 function TextGrid() {
   const handleInput = (e) => {
-    console.log(e.target.value);
+    let target = e.target;
+    let key = e.key;
+    if (key !== "Backspace") {
+      if (key === "Enter") {
+        if (!target.nextElementSibling && target.value.length === 1) {
+          //evaluate word logic goes here
+          if (target.parentNode.nextElementSibling)
+            target.parentNode.nextElementSibling.firstChild.focus();
+        }
+      } else if (target.nextElementSibling) {
+        target.nextElementSibling.focus();
+      }
+    } else if (e.target.previousElementSibling) {
+      target.previousElementSibling.focus();
+    }
   };
   return (
     <div id="textgrid" onKeyUp={handleInput}>
       <div className="row1 row">
-        <input className="item1 item" type="text" maxLength={1}></input>
+        <input
+          className="item1 item"
+          type="text"
+          autoFocus={true}
+          maxLength={1}
+        ></input>
         <input className="item2 item" type="text" maxLength={1}></input>
         <input className="item3 item" type="text" maxLength={1}></input>
         <input className="item4 item" type="text" maxLength={1}></input>
